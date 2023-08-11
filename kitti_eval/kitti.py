9c42e704970c620a193186438888107c4b2497e4
girish1511 <girishg@umich.edu>
2023-08-10 23:18:56 -0400

    evaluate algo performance on kitti

---
+++ kitti_eval/kitti.py
@@ -0,0 +1,118 @@
+"""
+Author: Luigi Piccinelli
+Licensed under the CC-BY NC 4.0 license (http://creativecommons.org/licenses/by-nc/4.0/)
+"""
+
+import os
+
+import numpy as np
+import torch
+from PIL import Image
+
+# from .dataset import BaseDataset
+from torch.utils.data import Dataset
+
+class KITTIDataset(Dataset):
+    
+    min_depth = 0.01
+    max_depth = 80
+
+    #Original
+    test_split = "kitti_eigen_test.txt"
+    # train_split = "kitti_eigen_train.txt"
+
+    #Raw Lidar - w/o 360
+    # train_split = "kitti_eigen_train_raw.txt"
+
+    #Raw Lidar - w/ 360
+    # train_split = "kitti360_eigen_train_raw.txt"
+
+    #Filtered Lidar w/o 360
+    train_split = "kitti_eigen_train_filter.txt"
+
+    #Raw+Semi
+    # train_split = "kitti360_eigen_train_raw+semi.txt"
+
+    
+
+    def __init__(
+        self,
+        semidense_file,
+        base_path,
+        depth_scale=256,
+        test_depth='raw',
+        **kwargs
+    ):
+        super().__init__()
+        self.base_path = base_path
+        self.depth_scale = depth_scale
+        self.test_depth = test_depth
+        self.split_file = semidense_file
+
+        # load annotations
+        self.dataset = []
+        self.load_dataset()
+        
+        print("Split file used: ", self.split_file)
+
+    def load_dataset(self):
+        self.invalid_depth_num = 0
+        with open(os.path.join(self.base_path, self.split_file)) as f:
+            for line in f:
+                img_info = dict()
+                # if not self.benchmark:  # benchmark test
+                depth_map = line.strip().split(" ")[1]
+                if depth_map == "None" or not os.path.exists(
+                    os.path.join(self.base_path, depth_map)
+                ):
+                    self.invalid_depth_num += 1
+                    continue
+                img_info["annotation_filename_depth"] = os.path.join(
+                    self.base_path, depth_map
+                )
+
+                self.dataset.append(img_info)
+
+        print(
+            f"Loaded {len(self.dataset)} images. Totally {self.invalid_depth_num} invalid pairs are filtered"
+        )
+
+    def __len__(self):
+        """Total number of samples of data."""
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        """Get training/test data after pipeline.
+        Args:
+            idx (int): Index of data.
+        Returns:
+            dict: Training/test data (with annotation if `test_mode` is set
+                False).
+        """
+        semidense_depth = (
+            np.asarray(
+                Image.open(
+                    os.path.join(
+                        self.base_path,
+                        self.dataset[idx]["annotation_filename_depth"],
+                    )
+                )
+            ).astype(np.float32)
+            / self.depth_scale
+        )
+
+        depth = (
+            np.asarray(
+                Image.open(
+                    os.path.join(
+                        self.base_path,
+                        self.dataset[idx]["annotation_filename_depth"].replace('groundtruth', f"groundtruth_{self.test_depth}"),
+                    )
+                )
+            ).astype(np.float32)
+            / self.depth_scale
+        )
+    
+        return semidense_depth, depth
+
+   
---
+++ kitti_eval/main.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+
+from kitti import KITTIDataset
+
+import numpy as np
+import torch
+from metrics import DICT_METRICS_DEPTH, RunningMetric
+
+from torch.utils.data import DataLoader, SequentialSampler
+
+from tqdm import tqdm
+
+def main(args):
+    min_depth = 0.01
+    max_depth = 80
+
+    eval_dataset = KITTIDataset(semidense_file=args.semidense_file, 
+                           base_path=args.data_path,
+                           test_depth=args.test_depth)
+
+    eval_sampler = SequentialSampler(eval_dataset)
+    eval_loader = DataLoader(
+        eval_dataset,
+        batch_size=1,
+        num_workers=4,
+        sampler=eval_sampler,
+        pin_memory=True,
+        drop_last=False
+    )
+
+    metrics_tracker = RunningMetric(list(DICT_METRICS_DEPTH.keys()))
+
+    for batch in tqdm(eval_loader):
+        semidense, depth = batch
+        mask = (depth > min_depth) & (semidense > min_depth)
+        mask = mask & (depth < max_depth) & (semidense < max_depth)
+
+        metrics_tracker.accumulate_metrics(gt=semidense, pred=depth, mask=mask)
+
+    print(metrics_tracker.get_metrics())
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Testing", conflict_handler="resolve")
+
+    parser.add_argument("--data-path", default=os.environ.get("TMPDIR", ""))
+    parser.add_argument("--semidense-file", type=str)
+    parser.add_argument("--test-depth", type=str, choices=['raw','filter'])
+    # parser.add_argument("--kitti", type=str, choices=['raw','clean','semidense'], required=False)
+    # parser.add_argument("--kitti_stereo", type=str, choices=['foreground','background','all'], required=False)
+    # parser.add_argument("--eval_set", type=str, choices=['kitti_stereo','kitti360', 'kitti'], required=False)
+    # parser.add_argument("--kitti360", type=str, choices=['raw','clean'], required=False)
+
+    args = parser.parse_args()
+    main(args)
